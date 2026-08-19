5ddb4184e7ed442d70221fdd4a7e9581420d5a6a
OKAN SARI <sari_okan@hotmail.com>
2026-08-19 14:36:42 +0300

    Add Brand repository

---
+++ app/Repositories/BrandRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Brand;
+use App\Repositories\Contracts\BrandRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class BrandRepository implements BrandRepositoryInterface
+{
+    public function all(): Collection
+    {
+        return Brand::query()
+            ->orderBy('name')
+            ->get();
+    }
+
+    public function find(int $id): Brand
+    {
+        return Brand::query()->findOrFail($id);
+    }
+
+    public function create(array $data): Brand
+    {
+        return Brand::query()->create($data);
+    }
+
+    public function update(
+        Brand $brand,
+        array $data
+    ): Brand {
+        $brand->update($data);
+
+        return $brand->refresh();
+    }
+
+    public function delete(Brand $brand): void
+    {
+        $brand->delete();
+    }
+}
