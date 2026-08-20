3b401cf529dc5f01352833bc7673315e28157f7e
OKAN SARI <sari_okan@hotmail.com>
2026-08-20 20:57:02 +0300

    Remove organization-location routes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ContractorController;
 use App\Http\Controllers\LocationBusinessEntityController;
 use App\Http\Controllers\OrganizationCompanyController;
-use App\Http\Controllers\OrganizationLocationController;
 use App\Http\Controllers\TenantOnboardingController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\BrandLocationController;
@@ -23,123 +22,36 @@ Route::get('/user', function (Request $request) {
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::post('logout', [AuthController::class, 'logout']);
-
-    /*
-    |--------------------------------------------------------------------------
-    | Tenant Management
-    |--------------------------------------------------------------------------
-    */
 
     Route::middleware('role:super-admin')->group(function () {
         Route::apiResource('tenants', TenantController::class);
-
-        Route::post(
-            'tenant-onboarding',
-            [TenantOnboardingController::class, 'store']
-        );
+        Route::post('tenant-onboarding', [TenantOnboardingController::class, 'store']);
     });
 
-    /*
-    |--------------------------------------------------------------------------
-    | Tenant Scoped API
-    |--------------------------------------------------------------------------
-    */
-
     Route::middleware('tenant')->group(function () {
-
         Route::middleware('role:super-admin')->group(function () {
-
             Route::apiResource('organizations', OrganizationController::class);
-
             Route::apiResource('companies', CompanyController::class);
-
             Route::apiResource('contractors', ContractorController::class);
-
             Route::apiResource('locations', LocationController::class);
-
             Route::apiResource('brands', BrandController::class);
+            Route::apiResource('locations.operational-units', OperationalUnitController::class);
 
-            Route::apiResource(
-                'locations.operational-units',
-                OperationalUnitController::class
-            );
+            Route::get('organizations/{organization}/companies', [OrganizationCompanyController::class, 'index']);
+            Route::put('organizations/{organization}/companies', [OrganizationCompanyController::class, 'sync']);
+            Route::post('organizations/{organization}/companies/{businessEntity}', [OrganizationCompanyController::class, 'attach']);
+            Route::delete('organizations/{organization}/companies/{businessEntity}', [OrganizationCompanyController::class, 'detach']);
 
-            Route::get(
-                'organizations/{organization}/companies',
-                [OrganizationCompanyController::class, 'index']
-            );
+            Route::get('brands/{brand}/locations', [BrandLocationController::class, 'index']);
+            Route::put('brands/{brand}/locations', [BrandLocationController::class, 'sync']);
+            Route::post('brands/{brand}/locations/{location}', [BrandLocationController::class, 'attach']);
+            Route::delete('brands/{brand}/locations/{location}', [BrandLocationController::class, 'detach']);
 
-            Route::put(
-                'organizations/{organization}/companies',
-                [OrganizationCompanyController::class, 'sync']
-            );
-
-            Route::post(
-                'organizations/{organization}/companies/{businessEntity}',
-                [OrganizationCompanyController::class, 'attach']
-            );
-
-            Route::delete(
-                'organizations/{organization}/companies/{businessEntity}',
-                [OrganizationCompanyController::class, 'detach']
-            );
-
-            Route::put(
-                'organizations/{organization}/locations',
-                [OrganizationLocationController::class, 'sync']
-            );
-
-            Route::post(
-                'organizations/{organization}/locations/{location}',
-                [OrganizationLocationController::class, 'attach']
-            );
-
-            Route::delete(
-                'organizations/{organization}/locations/{location}',
-                [OrganizationLocationController::class, 'detach']
-            );
-
-            Route::get(
-                'brands/{brand}/locations',
-                [BrandLocationController::class, 'index']
-            );
-
-            Route::put(
-                'brands/{brand}/locations',
-                [BrandLocationController::class, 'sync']
-            );
-
-            Route::post(
-                'brands/{brand}/locations/{location}',
-                [BrandLocationController::class, 'attach']
-            );
-
-            Route::delete(
-                'brands/{brand}/locations/{location}',
-                [BrandLocationController::class, 'detach']
-            );
-
-            Route::get(
-                'locations/{location}/business-entities',
-                [LocationBusinessEntityController::class, 'index']
-            );
-
-            Route::post(
-                'locations/{location}/business-entities',
-                [LocationBusinessEntityController::class, 'store']
-            );
-
-            Route::put(
-                'locations/{location}/business-entities/{businessEntity}',
-                [LocationBusinessEntityController::class, 'update']
-            );
-
-            Route::delete(
-                'locations/{location}/business-entities/{businessEntity}',
-                [LocationBusinessEntityController::class, 'destroy']
-            );
+            Route::get('locations/{location}/business-entities', [LocationBusinessEntityController::class, 'index']);
+            Route::post('locations/{location}/business-entities', [LocationBusinessEntityController::class, 'store']);
+            Route::put('locations/{location}/business-entities/{businessEntity}', [LocationBusinessEntityController::class, 'update']);
+            Route::delete('locations/{location}/business-entities/{businessEntity}', [LocationBusinessEntityController::class, 'destroy']);
         });
     });
 });
