9f317680dcde0c72079ff08b77642ba0f59b4dae
OKAN SARI <sari_okan@hotmail.com>
2026-08-20 21:18:07 +0300

    Remove brand onboarding from tenant onboarding validation

--- app/Http/Requests/TenantOnboardingRequest.php
+++ app/Http/Requests/TenantOnboardingRequest.php
@@ -15,22 +15,15 @@ class TenantOnboardingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            /*
-             * Onboarding type
-             */
             'onboarding_type' => [
                 'required',
                 Rule::in([
                     'holding',
                     'group',
                     'company',
-                    'brand',
                 ]),
             ],
 
-            /*
-             * Tenant
-             */
             'tenant' => [
                 'required',
                 'array',
@@ -48,9 +41,6 @@ class TenantOnboardingRequest extends FormRequest
                 'max:255',
             ],
 
-            /*
-             * Root Organization
-             */
             'organization' => [
                 'required',
                 'array',
@@ -62,11 +52,6 @@ class TenantOnboardingRequest extends FormRequest
                 'max:255',
             ],
 
-            /*
-             * Company
-             *
-             * Only required for company onboarding.
-             */
             'company' => [
                 'nullable',
                 'array',
@@ -89,30 +74,6 @@ class TenantOnboardingRequest extends FormRequest
                 ]),
             ],
 
-            /*
-             * Brand
-             *
-             * Only required for brand onboarding.
-             */
-            'brand' => [
-                'nullable',
-                'array',
-                'required_if:onboarding_type,brand',
-            ],
-
-            'brand.name' => [
-                'required_if:onboarding_type,brand',
-                'nullable',
-                'string',
-                'max:255',
-            ],
-
-            /*
-             * Location
-             *
-             * Company onboarding with an individual company type
-             * requires the initial center location.
-             */
             'location' => [
                 'nullable',
                 'array',
