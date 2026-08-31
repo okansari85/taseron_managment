bc5326231d12bd94711f4580762d2f957655a26a
OKAN SARI <sari_okan@hotmail.com>
2026-08-31 17:12:37 +0300

    Register Turkey reference seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-         $this->call([
+        $this->call([
             SuperAdminSeeder::class,
+            TurkeyLocationReferenceSeeder::class,
         ]);
     }
 }
