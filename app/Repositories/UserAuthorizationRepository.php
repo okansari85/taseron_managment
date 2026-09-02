b5ee534eb3bc07fdad1f88385d9e94141f05027d
OKAN SARI <sari_okan@hotmail.com>
2026-09-03 02:13:08 +0300

    Add user forbidden permission persistence

--- app/Repositories/UserAuthorizationRepository.php
+++ app/Repositories/UserAuthorizationRepository.php
@@ -11,18 +11,18 @@ class UserAuthorizationRepository
 {
     public function all(): Collection
     {
-        return User::query()->with(['roles', 'permissions', 'scopes'])->orderBy('name')->get();
+        return User::query()->with(['roles', 'permissions', 'forbiddenPermissions', 'scopes'])->orderBy('name')->get();
     }
 
     public function find(int $id): User
     {
-        return User::query()->with(['roles', 'permissions', 'scopes'])->findOrFail($id);
+        return User::query()->with(['roles', 'permissions', 'forbiddenPermissions', 'scopes'])->findOrFail($id);
     }
 
     public function assignRole(User $user, Role $role): User
     {
         $user->syncRoles([$role]);
-        return $user->load(['roles', 'permissions', 'scopes']);
+        return $user->load(['roles', 'permissions', 'forbiddenPermissions', 'scopes']);
     }
 
     public function syncDirectPermissions(User $user, array $permissionNames): User
@@ -37,6 +37,22 @@ class UserAuthorizationRepository
         }
 
         $user->syncPermissions($permissions->all());
-        return $user->load(['roles', 'permissions', 'scopes']);
+        return $user->load(['roles', 'permissions', 'forbiddenPermissions', 'scopes']);
+    }
+
+    public function syncForbiddenPermissions(User $user, array $permissionNames): User
+    {
+        $permissions = Permission::query()
+            ->whereIn('name', $permissionNames)
+            ->where('guard_name', $user->getDefaultGuardName())
+            ->get();
+
+        if ($permissions->count() !== count(array_unique($permissionNames))) {
+            abort(422, 'Bir veya daha fazla forbidden permission bulunamadı.');
+        }
+
+        $user->forbiddenPermissions()->sync($permissions->modelKeys());
+
+        return $user->load(['roles', 'permissions', 'forbiddenPermissions', 'scopes']);
     }
 }
