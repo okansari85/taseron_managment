7ceffb134037a0311ea771706e5d43a6e27d631b
OKAN SARI <sari_okan@hotmail.com>
2026-09-03 00:30:19 +0300

    feat: add location expert model

---
+++ app/Models/LocationExpert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LocationExpert extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['location_id', 'user_id'];
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
