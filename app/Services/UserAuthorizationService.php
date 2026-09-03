289bf41c3922d8ad0dfe587e540471e133f598ac
OKAN SARI <sari_okan@hotmail.com>
2026-09-03 15:58:30 +0300

    feat: add user creation to authorization service

--- app/Services/UserAuthorizationService.php
+++ app/Services/UserAuthorizationService.php
@@ -22,6 +22,25 @@ class UserAuthorizationService
         return $this->repository->find($id);
     }
 
+    public function create(string $name, string $email, string $password, ?string $roleName = null): User
+    {
+        $user = User::query()->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        if ($roleName !== null && $roleName !== '') {
+            $role = Role::query()
+                ->where('name', $roleName)
+                ->where('guard_name', $user->getDefaultGuardName())
+                ->firstOrFail();
+            $user->syncRoles([$role]);
+        }
+
+        return $this->repository->find($user->id);
+    }
+
     public function assignRole(User $user, string $roleName): User
     {
         $role = Role::query()
