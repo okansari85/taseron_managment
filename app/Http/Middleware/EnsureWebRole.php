1230b419039be8e8200c7822935f7f869209d028
OKAN SARI <sari_okan@hotmail.com>
2026-09-03 18:47:40 +0300

    Fix Sanctum role authorization guard bridge

--- app/Http/Middleware/EnsureWebRole.php
+++ app/Http/Middleware/EnsureWebRole.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureWebRole
@@ -16,12 +17,18 @@ class EnsureWebRole
             abort(401, 'User is not logged in.');
         }
 
-        foreach ($roles as $role) {
-            if ($user->hasRole($role, 'web')) {
-                return $next($request);
-            }
+        $hasRole = DB::table('model_has_roles')
+            ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
+            ->where('model_has_roles.model_id', $user->getKey())
+            ->where('model_has_roles.model_type', $user::class)
+            ->where('roles.guard_name', 'web')
+            ->whereIn('roles.name', $roles)
+            ->exists();
+
+        if (! $hasRole) {
+            abort(403, 'User does not have the required role.');
         }
 
-        abort(403, 'User does not have the required role.');
+        return $next($request);
     }
 }
