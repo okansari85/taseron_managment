f4f51ba9a46719f33c7625c502243f431490b4f8
OKAN SARI <sari_okan@hotmail.com>
2026-09-04 15:45:30 +0300

    Reduce role seed to six main roles

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -14,25 +14,17 @@ class RolePermissionSeeder extends Seeder
             'super-admin' => [
                 'tenant.view', 'tenant.manage', 'users.manage', 'roles.manage', 'reports.view',
             ],
-            'tenant-admin' => [
+            'tenant' => [
                 'organizations.manage', 'locations.manage', 'contractors.manage', 'users.manage', 'reports.view',
             ],
-            'tenant-manager' => [
-                'organizations.view', 'locations.manage', 'contractors.manage', 'visits.manage', 'documents.manage', 'reports.view',
-            ],
-            'isg-manager' => [
+            'isg' => [
                 'locations.view', 'contractors.view', 'visits.manage', 'documents.manage', 'findings.manage', 'reports.view',
             ],
-            'isg-user' => [
-                'locations.view', 'contractors.view', 'visits.create', 'documents.view', 'findings.create',
-            ],
-            'contractor-manager' => [
+            'contractor' => [
                 'contractor.view', 'personnel.manage', 'assets.manage', 'documents.manage', 'visits.view',
             ],
-            'contractor-user' => [
-                'contractor.view', 'personnel.manage', 'assets.manage', 'documents.upload', 'visits.view',
-            ],
-            'viewer' => [
+            'security' => [],
+            'operation' => [
                 'organizations.view', 'locations.view', 'contractors.view', 'visits.view', 'documents.view', 'reports.view',
             ],
         ];
