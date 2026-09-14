30061ba5175776d40005862d8c5995da677e2d8c
OKAN SARI <sari_okan@hotmail.com>
2026-09-14 23:54:02 +0300

    Add template Camelot fixture validation

--- app/Http/Requests/AnalyzeReportFileRequest.php
+++ app/Http/Requests/AnalyzeReportFileRequest.php
@@ -13,7 +13,7 @@ class AnalyzeReportFileRequest extends FormRequest
 
     public function rules(): array
     {
-        if ($this->boolean('gemini_fixture_v12') || $this->boolean('gemini_fixture_get')) {
+        if ($this->boolean('gemini_fixture_v12') || $this->boolean('gemini_fixture_get') || $this->boolean('gemini_fixture_camelot') || $this->boolean('gemini_fixture_delete')) {
             return [
                 'fixture_id' => ['required', 'uuid'],
             ];
