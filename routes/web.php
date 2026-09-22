fe705959d0a78e40d61aea5c7bf8b0e853c92d8c
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 17:30:40 +0300

    Add expert onboarding routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\ExpertController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/api/experts/set-password', [ExpertController::class, 'setPassword']);
+
+Route::middleware(['auth:sanctum', 'web-role:super-admin'])->group(function () {
+    Route::post('/api/experts', [ExpertController::class, 'store']);
 });
