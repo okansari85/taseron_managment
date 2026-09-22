44eea5ca612285696d87133cdff6ebe90611c489
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 17:33:32 +0300

    Keep web routes unchanged

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\ExpertController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::post('/api/experts/set-password', [ExpertController::class, 'setPassword']);
-
-Route::middleware(['auth:sanctum', 'web-role:super-admin'])->group(function () {
-    Route::post('/api/experts', [ExpertController::class, 'store']);
-    Route::post('/api/experts/{tenant}/impersonate', [ExpertController::class, 'impersonate']);
 });
