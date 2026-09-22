2502fe16ad1d55eba7b929bb885c6d222d0b2229
OKAN SARI <sari_okan@hotmail.com>
2026-09-22 17:45:33 +0300

    Add expert invitation email notification

---
+++ app/Notifications/ExpertInvitationNotification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ExpertInvitationNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(private string $token)
+    {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = rtrim(config('app.frontend_url'), '/')
+            . '/set-password?token=' . urlencode($this->token)
+            . '&email=' . urlencode($notifiable->email);
+
+        return (new MailMessage)
+            ->subject('PKTakip uzman hesabınız hazır')
+            ->greeting('Merhaba ' . $notifiable->name . ',')
+            ->line('PKTakip uzman hesabınız yönetici tarafından oluşturuldu.')
+            ->line('Sisteme giriş yapabilmek için aşağıdaki bağlantıdan kendi şifrenizi belirleyin.')
+            ->action('Şifremi Belirle', $url)
+            ->line('Bu bağlantı 60 dakika boyunca geçerlidir.')
+            ->line('Bu e-postayı siz beklemiyorsanız dikkate almayabilirsiniz.');
+    }
+}
