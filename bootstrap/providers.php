6f1608321972d85cbd66bcc6cc685cb2bb5b530e
OKAN SARI <sari_okan@hotmail.com>
2026-09-23 15:44:19 +0300

    feat: register customer routes provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\CustomerRoutesServiceProvider;
 use App\Providers\TenancyServiceProvider;
 
 
 return [
     AppServiceProvider::class,
     TenancyServiceProvider::class,
+    CustomerRoutesServiceProvider::class,
 
 ];
